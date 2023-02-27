1fac3493e1c29e22b67a721a1154f74cc0921205
hutu <104620424+hu-tu-yi-shi@users.noreply.github.com>
2023-02-27 23:10:59 +0800

    add eslint, prettier

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,30 +1,41 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
 
 const Post = defineDocumentType(() => ({
-    name: 'Post',
-    filePathPattern: `posts/**/*.mdx`,
-    contentType: 'mdx',
-    fields: {
-        title: {
-            type: 'string',
-            description: 'The title of the post',
-            required: true,
-        },
-        date: {
-            type: 'date',
-            description: 'The date of the post',
-            required: true,
-        },
+  name: "Post",
+  filePathPattern: `posts/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      description: "The title of the post",
+      required: true,
     },
-    computedFields: {
-        url: {
-            type: 'string',
-            resolve: (doc) => `/posts/${doc._raw.flattenedPath}`,
-        },
+    date: {
+      type: "date",
+      description: "The date of the post",
+      required: true,
     },
-}))
+    excerpt: {
+      type: "string",
+      description: "Short summary of the post",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+      description: "Tags for the post",
+      required: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (doc) => `/posts/${doc._raw.flattenedPath}`,
+    },
+  },
+}));
 
 export default makeSource({
-    contentDirPath: 'src/content',
-    documentTypes: [Post],
-})
+  contentDirPath: "src/content",
+  documentTypes: [Post],
+});
