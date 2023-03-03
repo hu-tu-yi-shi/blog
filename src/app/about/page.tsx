67893df08458b2df03ebea909abe98babfaf11e9
hutu <104620424+hu-tu-yi-shi@users.noreply.github.com>
2023-03-03 17:49:16 +0800

    add page about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React from 'react'
+import { Button, Descriptions } from 'antd'
+import { Space } from 'antd'
+
+const Index = () => {
+    return (
+        <div>
+            <Space size={'large'} direction={'vertical'}>
+                <Descriptions title="关于我" column={1}>
+                    <Descriptions.Item label="昵称">糊涂</Descriptions.Item>
+                    <Descriptions.Item label="微信">****</Descriptions.Item>
+                    <Descriptions.Item label="技术栈">
+                        JavaScript、TypeScript、CSS
+                    </Descriptions.Item>
+
+                    <Descriptions.Item label="框架">next.js</Descriptions.Item>
+                    <Descriptions.Item label="感兴趣">
+                        web3、区块链
+                    </Descriptions.Item>
+                </Descriptions>
+
+                <Descriptions title="我的作品" column={1}>
+                    <Descriptions.Item label="111111">11111</Descriptions.Item>
+                    <Descriptions.Item label="111111">11111</Descriptions.Item>
+                    <Descriptions.Item label="111111">11111</Descriptions.Item>
+                    <Descriptions.Item label="111111">11111</Descriptions.Item>
+                    <Descriptions.Item label="111111">11111</Descriptions.Item>
+                </Descriptions>
+            </Space>
+        </div>
+    )
+}
+
+export default Index
