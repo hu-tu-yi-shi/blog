a5443cf923f46382b1cbfb28a0f2d835b146afb9
hutu <104620424+hu-tu-yi-shi@users.noreply.github.com>
2023-03-03 18:06:03 +0800

    格式化代码

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,8 +7,6 @@ const Index = () => {
     return (
         <div>
             <Space size={'large'} direction={'vertical'}>
-
-
                 <Descriptions title="我的作品" column={1}>
                     <Descriptions.Item label="111111">11111</Descriptions.Item>
                     <Descriptions.Item label="111111">11111</Descriptions.Item>
@@ -16,7 +14,6 @@ const Index = () => {
                     <Descriptions.Item label="111111">11111</Descriptions.Item>
                     <Descriptions.Item label="111111">11111</Descriptions.Item>
                 </Descriptions>
-
                 需要完善，给项目添加足够的说明，如技术栈、思路 等等
             </Space>
         </div>
