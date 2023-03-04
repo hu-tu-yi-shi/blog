54b2bf602358fc29c462faed0995a5ea8f05fb44
hutu <104620424+hu-tu-yi-shi@users.noreply.github.com>
2023-03-04 16:22:54 +0800

    fix: test

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+// 目前webstorm 不支持检测  eslint.config.js ,等以后再弄吧
+
+
+// https://www.jetbrains.com/help/webstorm/eslint.html#ws_js_eslint_activate  支持的配置文件名
+
+// 等他支持后， 就可以模仿这些配置 https://github.com/search?q=filename%3Aeslint.config.js+next+js
+
+
+// 正在等官方修复，估计快了 https://youtrack.jetbrains.com/issue/WEB-57661
